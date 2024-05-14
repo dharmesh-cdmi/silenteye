7ae7d51876f24f47ab9bb95e6612530f04708f89
coadanas <coadanas@gmail.com>
2024-05-14 21:04:29 +0530

    update apps, location, notes & export

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ import KeyLogger from "./Pages/KeyLogger";
 import { SavedPassword } from "./Pages/SavedPassword";
 import DeviceControl from "./Pages/DeviceControl.jsx";
 
+import AllApps from "./Pages/Apps/AllApps.js"
+import TotalApp from "./Pages/Apps/TotalApps.js"
+import InstalledApp from "./Pages/Apps/InstalledApp.js"
+import UnInstalledApp from "./Pages/Apps/UnstalledApp.js"
+import BlockedApp from "./Pages/Apps/BlockedApp.js"
+import "./assests/css/App.css"
+
+import Location from "./Pages/Location/Location.js"
+import LocationHistory from "./Pages/Location/LocationHistory.js"
+import GeoFence from "./Pages/Location/geoFence.js"
+
+import Export from "./Pages/Export/Export.js"
+
 
 function App() {
   const router = createBrowserRouter([
@@ -54,7 +67,107 @@ function App() {
           element: <KeyLogger/>,
           },    
       ],
-    }
+    },
+        {
+      path: "/notes",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <Notes/>,
+        },    
+      ],
+    },
+    {
+      path: "/apps",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <AllApps/>,
+        },    
+      ],
+    },
+    {
+      path: "/apps/installedapp",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <InstalledApp/>,
+        },    
+      ],
+    },
+    {
+      path: "/apps/uninstalledapp",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <UnInstalledApp/>,
+        },    
+      ],
+    },
+    {
+      path: "/apps/blockedapp",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <BlockedApp/>,
+        },    
+      ],
+    },
+    {
+      path: "/location",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <Location/>,
+        },    
+      ],
+    },
+    {
+      path: "/location/livelocation",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <Location/>,
+        },    
+      ],
+    },
+    {
+      path: "/location/locationhistory",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <LocationHistory/>,
+        },    
+      ],
+    },
+    {
+      path: "/location/geofrence",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <GeoFence/>,
+        },    
+      ],
+    },
+    {
+      path: "/export",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <Export/>,
+        },    
+      ],
+    },
   ]);
   return (
     <>
---
+++ src/Pages/Location/LocationData/LocationData.js
@@ -0,0 +1,22 @@
+export const LocationData = [
+  {
+    title: "375 James st, New Haven, CT 06513",
+    des: "41.3130335, -72.9037375"
+  },
+  {
+    title: "375 James st, New Haven, CT 06513",
+    des: "41.3130335, -72.9037375"
+  },
+  {
+    title: "375 James st, New Haven, CT 06513",
+    des: "41.3130335, -72.9037375"
+  },
+  {
+    title: "375 James st, New Haven, CT 06513",
+    des: "41.3130335, -72.9037375"
+  },
+  {
+    title: "375 James st, New Haven, CT 06513",
+    des: "41.3130335, -72.9037375"
+  }
+];
