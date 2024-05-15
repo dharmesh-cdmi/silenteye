4b5841ac01651f0e429a8ffcb2964d95d01c7a62
coadanas <coadanas@gmail.com>
2024-05-15 15:21:09 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layout/RootLayout";
-
-import Notes from "./Pages/Notes/Notes.js"
-import "./assests/css/App.css"
+import Notes from "./Pages/Notes/Notes.js";
+import "./assests/css/App.css";
 import Dashboard from "./Pages/Dashboard";
+import DeviceControl from "./Pages/DeviceControl";
 import KeyLogger from "./Pages/KeyLogger";
 import { SavedPassword } from "./Pages/SavedPassword";
-import DeviceControl from "./Pages/DeviceControl.jsx";
+import SocialApps from "./Pages/SocialApps";
+import AllAllert from "./Pages/AllAllert";
+import NotificationSetting from "./Pages/NotificationSetting";
+import InstantAlert from "./Pages/InstantAlert.jsx";
+import AppPermission from "./Pages/AppPermission";
+import PermisionManager from "./Pages/PermisionManager";
+import Payment from "./Pages/Payment.jsx";
+import Callhistory from "./Pages/Calls/Callhistory.jsx";
+import BlockedCalls from "./Pages/Calls/BlockedCalls.jsx";
+import FaceTime from "./Pages/Calls/FaceTime.jsx";
+import ForwardCalls from "./Pages/Calls/ForwardCalls.jsx";
+import ContactList from "./Pages/Contacts/ContactList.jsx";
+import BlockedContact from "./Pages/Contacts/BlockedContact.jsx";
+import InstalledApp from "./Pages/Apps/InstalledApp.js";
+import UnInstalledApp from "./Pages/Apps/UnstalledApp.js";
+import BlockedApp from "./Pages/Apps/BlockedApp.js";
 
-import AllApps from "./Pages/Apps/AllApps.js"
-import TotalApp from "./Pages/Apps/TotalApps.js"
-import InstalledApp from "./Pages/Apps/InstalledApp.js"
-import UnInstalledApp from "./Pages/Apps/UnstalledApp.js"
-import BlockedApp from "./Pages/Apps/BlockedApp.js"
-import "./assests/css/App.css"
 
 import Location from "./Pages/Location/Location.js"
 import LocationHistory from "./Pages/Location/LocationHistory.js"
@@ -26,6 +35,8 @@ import BrowseDataUsage from "./Pages/Browser/BrowseDataUsage.js"
 import WebFilter from "./Pages/Browser/WebFilter.js"
 import ScreenTime from "./Pages/Browser/ScreenTime.js"
 
+import AllApps from "./Pages/Apps/AllApps.js"
+
 function App() {
   const router = createBrowserRouter([
     {
@@ -35,188 +46,136 @@ function App() {
         {
           index: true,
           element: <Dashboard />,
-          },  
-          {
-            path: "/devicecontrol",
-            element: <DeviceControl />,
-            },  
-               
-               
-      ],
-
-    },
-    {
-      path: "/SavedPassword",
-      element: <RootLayout />,
-      children: [
+        },
         {
-          index: true,
-          element: <SavedPassword/>,
-          },    
-      ],
-    },
-
-
-
-
-
-
-
-    {
-      path: "/KeyLogger",
-      element: <RootLayout />,
-      children: [
+          path: "/devicecontrol",
+          element: <DeviceControl />,
+        },
+        // call Routes
         {
-          index: true,
-          element: <KeyLogger/>,
-          },    
-      ],
-    },
+          path: "/calls/callhistory",
+          element: <Callhistory />,
+        },
+        {
+          path: "/calls/blockedcalls",
+          element: <BlockedCalls />,
+        },
+        {
+          path: "/calls/facetime",
+          element: <FaceTime />,
+        },
+        {
+          path: "/calls/forwardedcalls",
+          element: <ForwardCalls />,
+        },
+        // Contacts Routes
+
+        {
+          path: "/contacts/contactslist",
+          element: <ContactList />,
+        },
+        {
+          path: "/contacts/blockedcontacts",
+          element: <BlockedContact />,
+        },
+
+        {
+          path: "/SavedPassword",
+          element: <SavedPassword />,
+        },
+
+        {
+          path: "/KeyLogger",
+          element: <KeyLogger />,
+        },
+        {
+          path: "/SocialApps",
+          element: <SocialApps />,
+        },
+        {
+          path: "/AllAllerts",
+          element: <AllAllert />,
+        },
+        {
+          path: "/NotificationSettings",
+          element: <NotificationSetting />,
+        },
+        {
+          path: "/InstantAlerts",
+          element: <InstantAlert />,
+        },
+        {
+          path: "/AppPermisions",
+          element: <AppPermission />,
+        },
+        {
+          path: "/PermisionManager",
+          element: <PermisionManager />,
+        },
+        // NOTES - APPS - EXORT -browser
         {
       path: "/notes",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <Notes/>,
-        },    
-      ],
+          element: <Notes />,
     },
     {
-      path: "/apps/allapps",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
+      path: "/Apps/allapp",
           element: <AllApps/>,
-        },    
-      ],
     },
     {
       path: "/apps/installedapp",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <InstalledApp/>,
-        },    
-      ],
     },
     {
       path: "/apps/uninstalledapp",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <UnInstalledApp/>,
-        },    
-      ],
     },
     {
       path: "/apps/blockedapp",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <BlockedApp/>,
-        },    
-      ],
     },
     {
       path: "/location",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <Location/>,
-        },    
-      ],
     },
     {
       path: "/location/livelocation",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <Location/>,
-        },    
-      ],
     },
     {
       path: "/location/locationhistory",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <LocationHistory/>,
-        },    
-      ],
     },
     {
       path: "/location/geofrence",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <GeoFence/>,
-        },    
-      ],
     },
     {
       path: "/export",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <Export/>,
-        },    
-      ],
     },
     {
       path: "/browser/browseHistory",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <BrowseHistory />,
-        },    
-      ],
     },
     {
       path: "/browser/datausage",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <BrowseDataUsage />,
-        },    
-      ],
     },
     {
       path: "/browser/webfilter",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
           element: <WebFilter />,
-        },    
-      ],
     },
     {
       path: "/browser/screentime",
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <ScreenTime />,
-        },    
+      element: <ScreenTime />,
+    },
       ],
     },
   ]);
   return (
     <>
-    <RouterProvider router={router} /> 
-  </>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
