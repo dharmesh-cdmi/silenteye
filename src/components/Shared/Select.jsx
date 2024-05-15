34bd0cc12fec674bc4b6373fc26bce632b5927c9
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-05-15 12:24:22 +0500

    all done

--- src/components/Shared/Select.jsx
+++ src/components/Shared/Select.jsx
@@ -1,37 +1,25 @@
-import React from "react";
-import Modal from "../Shared/Modal";
-import Upgrade from "../Shared/Upgrade";
+import React from 'react'
 
-const Select = ({ label, register, options, onclick }) => {
+const Select = ({label,register,options,onclick}) => {
   return (
-    <div className=" w-full">
-      <label className="block text-[16px] text-[#000000] opacity-[50%]">
-        {label}
-      </label>
-      <Modal>
-        <Modal.Toggle toggleName="delete-keylogger">
-          <select
-            onclick={onclick}
-            className="w-full p-[16px]  rounded-[12px]  bg-[#8E8E9314] mt-1 cursor-pointer"
-            {...register}
-          >
-            {options.map((option) => (
-              <option
-                key={option.value}
-                value={option.value}
-                className=" font-medium  hidden "
-              >
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </Modal.Toggle>
-        <Modal.Window windowName="delete-keylogger">
-          <Upgrade />
-        </Modal.Window>
-      </Modal>
+    <div className=' w-full'>
+        <label className="block text-[16px] text-[#000000] opacity-[50%]">
+          {label}
+        </label> 
+        <select
+        onclick={onclick}
+        className="w-full p-[16px]  rounded-[12px]  bg-[#8E8E9314] mt-1 cursor-pointer"
+        {...register}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value} className='text-[18px] text-[#000000] font-medium  '>
+            {option.label}
+          </option>
+        ))}
+      </select>  
+    
     </div>
-  );
-};
+  )
+}
 
-export default Select;
+export default Select
