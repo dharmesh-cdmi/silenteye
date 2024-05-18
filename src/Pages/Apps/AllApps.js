587edab8a6ca987d436925b6a3c1b40157c0f727
coadanas <147676534+coadanas@users.noreply.github.com>
2024-05-19 00:51:22 +0530

    Update AllApps.js

--- src/Pages/Apps/AllApps.js
+++ src/Pages/Apps/AllApps.js
@@ -1,1 +1,16 @@
 import {useState} from "react"
+import { Link } from 'react-router-dom';
+import "./AllApps.css"
+
+
+// FILES
+import TotalApps from "./TotalApps.js"
+import ExportBtn from "../../components/pageExportBtn/PageExportBtn.jsx"
+
+const AllApps=()=>{
+  return(
+  <TotalApps />
+    )
+}
+
+export default AllApps
