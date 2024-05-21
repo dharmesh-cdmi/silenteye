f71309d87989ed2d8f9b314ec153e74c14cfcd9b
coadanas <147676534+coadanas@users.noreply.github.com>
2024-05-22 02:49:26 +0530

    Update LocationHead.js

--- src/components/LocationHead/LocationHead.js
+++ src/components/LocationHead/LocationHead.js
@@ -1,41 +1,52 @@
 import {useState} from "react"
 import { Link } from 'react-router-dom';
 import { useMoveBack } from "../../hooks/useMoveBack.js";
-import "./Locationhead.css"
+import "../BrowserHead/BrowserHead.css"
 
 import { IoFilterSharp } from "react-icons/io5";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { TbFileExport } from "react-icons/tb";
 import { IoIosSearch } from "react-icons/io";
 
+import Modal from '../Shared/Modal.jsx';
+import Upgrade from '../Shared/Upgrade';
+
 // FILES
+import ExportBtn from "../../components/pageExportBtn/PageExportBtn.jsx"
 
 const LocationHead=()=>{
 const moveBacks = useMoveBack();
   return(
-    <div className="Location-nav flex justify-between align-middle">
+    <div className="Browser-nav flex justify-between align-middle">
     {/* nav left*/}
-    <div className="Location-nav-left flex justify-center align-bottom">
-         <button onClick={moveBacks} className="notNavLeftIcon shadow-[0px_0px_56px_0px_#00000014] "><FaArrowLeftLong /></button>
-       <span>Location</span>
+    <div className="Browser-nav-left flex justify-center align-bottom">
+         <button onClick={moveBacks} className="notNavLeftIcon shadow-lg "><FaArrowLeftLong /></button>
+       <span>Browser</span>
     </div>
    
     {/* nav right*/}
-  <div className="Location-nav-right flex justify-center items-center p-4">
+  <div className="Browser-nav-right flex justify-center items-center p-4">
       {/* Search */}
-      <span className="flex items-center LocationNavRightSearch cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
+      <span className="flex items-center BrowserNavRightSearch cursor-pointer shadow-lg ">
         <IoIosSearch />
-        <input type="text" placeholder="Search Location" />
+        <input type="text" placeholder="Search Browser" />
       </span>
 
       {/* Export */}
-      <span className="flex items-center LocationNavRightExportIcon cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
+          <Modal>
+           <Modal.Toggle toggleName="Upgrade">
+      <span className="flex items-center LocationNavRightExportIcon cursor-pointer shadow-lg ">
         <TbFileExport />
         <span className="responsiveTitleHide">Export</span>
       </span>
+         </Modal.Toggle>
+          <Modal.Window windowName="Upgrade">
+            <Upgrade />
+          </Modal.Window>
+          </Modal>
 
       {/* Filter */}
-      <span className="flex items-center LocationNavRightFilterIcon cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
+      <span className="flex items-center BrowserNavRightFilterIcon cursor-pointer shadow-lg ">
         <IoFilterSharp />
         <span className="responsiveTitleHide">Filter</span>
       </span>
