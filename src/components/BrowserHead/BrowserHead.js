fd944311220ecd44ee6be27ea5a451c070ccf020
coadanas <147676534+coadanas@users.noreply.github.com>
2024-05-22 02:48:51 +0530

    Update BrowserHead.js

--- src/components/BrowserHead/BrowserHead.js
+++ src/components/BrowserHead/BrowserHead.js
@@ -8,6 +8,9 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import { TbFileExport } from "react-icons/tb";
 import { IoIosSearch } from "react-icons/io";
 
+import Modal from '../Shared/Modal.jsx';
+import Upgrade from '../Shared/Upgrade';
+
 // FILES
 import ExportBtn from "../../components/pageExportBtn/PageExportBtn.jsx"
 
@@ -17,26 +20,33 @@ const moveBacks = useMoveBack();
     <div className="Browser-nav flex justify-between align-middle">
     {/* nav left*/}
     <div className="Browser-nav-left flex justify-center align-bottom">
-         <button onClick={moveBacks} className="notNavLeftIcon shadow-[0px_0px_56px_0px_#00000014] "><FaArrowLeftLong /></button>
+         <button onClick={moveBacks} className="notNavLeftIcon shadow-lg "><FaArrowLeftLong /></button>
        <span>Browser</span>
     </div>
    
     {/* nav right*/}
   <div className="Browser-nav-right flex justify-center items-center p-4">
       {/* Search */}
-      <span className="flex items-center BrowserNavRightSearch cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
+      <span className="flex items-center BrowserNavRightSearch cursor-pointer shadow-lg ">
         <IoIosSearch />
         <input type="text" placeholder="Search Browser" />
       </span>
 
       {/* Export */}
-      <span className="flex items-center BrowserNavRightExportIcon cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
+          <Modal>
+           <Modal.Toggle toggleName="Upgrade">
+      <span className="flex items-center LocationNavRightExportIcon cursor-pointer shadow-lg ">
         <TbFileExport />
         <span className="responsiveTitleHide">Export</span>
       </span>
+         </Modal.Toggle>
+          <Modal.Window windowName="Upgrade">
+            <Upgrade />
+          </Modal.Window>
+          </Modal>
 
       {/* Filter */}
-      <span className="flex items-center BrowserNavRightFilterIcon cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
+      <span className="flex items-center BrowserNavRightFilterIcon cursor-pointer shadow-lg ">
         <IoFilterSharp />
         <span className="responsiveTitleHide">Filter</span>
       </span>
