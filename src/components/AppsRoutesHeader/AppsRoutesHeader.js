85203aa80c1f5743b23ed13dde17175cf3010752
coadanas <147676534+coadanas@users.noreply.github.com>
2024-05-21 20:34:15 +0530

    Update AppsRoutesHeader.js

--- src/components/AppsRoutesHeader/AppsRoutesHeader.js
+++ src/components/AppsRoutesHeader/AppsRoutesHeader.js
@@ -15,7 +15,7 @@ const AppsRoutesHeader = () => {
       <div className="Apps-nav flex justify-between align-middle">
         {/* nav left*/}
         <div className="Apps-nav-left flex justify-center align-bottom">
-          <button className="notNavLeftIcon">
+          <button className="notNavLeftIcon shadow-[0px_0px_56px_0px_#00000014] ">
             <FaArrowLeftLong />
           </button>
           <span>Apps</span>
@@ -24,19 +24,19 @@ const AppsRoutesHeader = () => {
         {/* nav right*/}
         <div className="Apps-nav-right flex justify-center items-center p-4">
           {/* Search */}
-          <span className="flex items-center appsNavRightSearch cursor-pointer">
+          <span className="flex items-center appsNavRightSearch cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
             <IoIosSearch />
             <input type="text" placeholder="Search Apps" />
           </span>
 
           {/* Export */}
-          <span className="flex items-center appsNavRightExportIcon cursor-pointer">
+          <span className="flex items-center appsNavRightExportIcon cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
             <TbFileExport />
             <span className="responsiveTitleHide">Export</span>
           </span>
 
           {/* Filter */}
-          <span className="flex items-center appsNavRightFilterIcon cursor-pointer">
+          <span className="flex items-center appsNavRightFilterIcon cursor-pointer shadow-[0px_0px_56px_0px_#00000014] ">
             <IoFilterSharp />
             <span className="responsiveTitleHide">Filter</span>
           </span>
