e20dd46fefe41a8b51b2bda317317be942c9e531
Saad Ullah Khan <saaad.khaan.69@gmail.com>
2024-05-22 21:11:59 +0500

    icons fixed

--- src/Pages/Apps/BlockedApp.js
+++ src/Pages/Apps/BlockedApp.js
@@ -1,38 +1,37 @@
 import React, { useState } from "react";
-import ExportBtn from "../../components/pageExportBtn/PageExportBtn.jsx"
-import AppsRoutesHeader from "../../components/AppsRoutesHeader/AppsRoutesHeader.js"
+import ExportBtn from "../../components/pageExportBtn/PageExportBtn.jsx";
+import AppsRoutesHeader from "../../components/AppsRoutesHeader/AppsRoutesHeader.js";
 
 import { BlockedAppData } from "./AppsData/BlockedAppData.js";
 import { BsWhatsapp } from "react-icons/bs";
 
-import Blogger from "../../assests/images/blogger.svg"
+import Blogger from "../../assests/images/blogger.svg";
 
 const BlockededApp = () => {
   const [appData, setAppData] = useState(BlockedAppData);
 
   return (
     <>
-    <div className="Apps-container">
-     <AppsRoutesHeader />
-     
-      <div className="appsComponentsCon lg:ms-auto ms-5  flex">
-        {appData && appData.length > 0 ? (
-          appData.map((v) => (
-            <div key={v.id} className="appBox flex flex-col justify-center">
-              <span className="appBoxicon">
-                {v.icon}              </span>
-              <div className="appBoxDes flex flex-col justify-center">
-                <h3>{v.title}</h3>
-                <p className="text-slate-600">{v.size}</p>
+      <div className="Apps-container">
+        <AppsRoutesHeader />
+
+        <div className="appsComponentsCon lg:ms-auto ms-5  flex">
+          {appData && appData.length > 0 ? (
+            appData.map((v) => (
+              <div key={v.id} className="appBox flex flex-col justify-center">
+                <span className="appBoxicon">{v.icon} </span>
+                <div className="appBoxDes flex flex-col justify-center">
+                  <h3>{v.title}</h3>
+                  <p className="text-slate-600">{v.size}</p>
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <h3>Data Not Found</h3>
-        )}
+            ))
+          ) : (
+            <h3>Data Not Found</h3>
+          )}
+        </div>
+        <ExportBtn />
       </div>
-      <ExportBtn />
-    </div>
     </>
   );
 };
--- src/components/network/mobile/components/Tabs.js
+++ src/components/network/mobile/components/Tabs.js
@@ -4,7 +4,8 @@ import { RiDualSim1Fill, RiDualSim2Fill } from "react-icons/ri";
 import { BsFillPencilFill } from "react-icons/bs";
 import Modal from "../../../Shared/Modal";
 import Upgrade from "../../../Shared/Upgrade";
-
+import sim from "../../../../assests/images/DashControl/T-Mobile.png";
+import sim2 from "../../../../assests/images/DashControl/Verizon.png";
 
 function Tabs() {
   return (
@@ -14,7 +15,7 @@ function Tabs() {
           <div className="flex justify-between align-middle">
             <div className="flex flex-col">
               <div className="flex">
-                <img className="h-8 w-8"></img>
+                <img className="h-6 w-6 mr-2" src={sim}></img>
                 <h3 className="font-semibold text-lg">Verizon</h3>
               </div>
               <div className="flex">
@@ -32,7 +33,7 @@ function Tabs() {
           <div className="flex justify-between align-middle">
             <div className="flex flex-col">
               <div className="flex">
-                <img className="h-8 w-8"></img>
+                <img className="h-6 w-6  mr-2" src={sim2}></img>
                 <h3 className="font-semibold text-lg">T-Mobile</h3>
               </div>
               <div className="flex">
@@ -52,25 +53,24 @@ function Tabs() {
             <div className="grid grid-cols-3 bg-gray-300 h-10 rounded-lg cursor-pointer">
               <Modal>
                 <Modal.Toggle toggleName="Update">
-              <div className="bg-green-600 rounded-lg m-1 flex justify-center items-center">
-                <RiDualSim1Fill className="w-7 h-7 " />
-              </div>
-              </Modal.Toggle>
+                  <div className="bg-green-600 rounded-lg m-1 flex justify-center items-center">
+                    <RiDualSim1Fill className="w-7 h-7 " />
+                  </div>
+                </Modal.Toggle>
 
-              <Modal.Toggle toggleName="Update">
-
-              <div className="flex justify-center items-center">      
-                <RiDualSim2Fill className="w-7 h-7" />
-              </div>
-             </Modal.Toggle>
-             <Modal.Toggle toggleName="Update">
-              <div className="flex justify-center items-center">
-                <BsFillPencilFill className="w-6 h-6" />
-              </div>
-              </Modal.Toggle>
-              <Modal.Window windowName="Update">
-                      <Upgrade/>
-                    </Modal.Window>
+                <Modal.Toggle toggleName="Update">
+                  <div className="flex justify-center items-center">
+                    <RiDualSim2Fill className="w-7 h-7" />
+                  </div>
+                </Modal.Toggle>
+                <Modal.Toggle toggleName="Update">
+                  <div className="flex justify-center items-center">
+                    <BsFillPencilFill className="w-6 h-6" />
+                  </div>
+                </Modal.Toggle>
+                <Modal.Window windowName="Update">
+                  <Upgrade />
+                </Modal.Window>
               </Modal>
             </div>
           </div>
@@ -80,20 +80,19 @@ function Tabs() {
             <p className="text-gray-500 text-sm">Default for Internet Data</p>
             <div className="grid grid-cols-2 bg-gray-300 h-10 rounded-lg cursor-pointer">
               <Modal>
-            <Modal.Toggle toggleName="Update">
-              <div className="bg-green-600 rounded-lg m-1 flex justify-center">
-              <RiDualSim1Fill className="w-7 h-7 " />
-
-              </div>
-              </Modal.Toggle>
-              <Modal.Toggle toggleName="Update">
-              <div className="flex justify-center items-center">
-                <RiDualSim2Fill className="w-7 h-7" />
-              </div>
-              </Modal.Toggle>
-              <Modal.Window windowName="Update">
-                      <Upgrade/>
-                    </Modal.Window>
+                <Modal.Toggle toggleName="Update">
+                  <div className="bg-green-600 rounded-lg m-1 flex justify-center">
+                    <RiDualSim1Fill className="w-7 h-7 " />
+                  </div>
+                </Modal.Toggle>
+                <Modal.Toggle toggleName="Update">
+                  <div className="flex justify-center items-center">
+                    <RiDualSim2Fill className="w-7 h-7" />
+                  </div>
+                </Modal.Toggle>
+                <Modal.Window windowName="Update">
+                  <Upgrade />
+                </Modal.Window>
               </Modal>
             </div>
           </div>
