3524c00f2d1fed74ccbcd8da1f012bc5157fe42d
coadanas <147676534+coadanas@users.noreply.github.com>
2024-05-24 14:53:04 +0530

    Update PageExportBtn.jsx

--- src/components/pageExportBtn/PageExportBtn.jsx
+++ src/components/pageExportBtn/PageExportBtn.jsx
@@ -1,25 +1,23 @@
 import {Link} from "react-router-dom"
 import  { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 import "./PageExportBtn.css"
-import Modal from "../Shared/Modal";
-import Upgrade from "../Shared/Upgrade";
+import Modal from "../Shared/Modal.jsx";
+import Upgrade from "../Shared/Upgrade.jsx";
 
 const PageExportBtn=()=>{
   return(
-
-    
             <Modal>
-          <Modal.Toggle toggleName="Upgrade">
+          <Modal.Toggle toggleName="export">
    
            <div className="notesViewMoreCon flex justify-center">
     <button className="flex justify-center shadow-lg">View More <MdOutlineKeyboardDoubleArrowDown /></button>
     </div>
             
-          </Modal.Toggle>
-          <Modal.Window windowName="viewMore-keylogger">
+         </Modal.Toggle>
+          <Modal.Window windowName="Upgrade">
             <Upgrade />
           </Modal.Window>
-        </Modal>
+          </Modal>
     )
 }
 
