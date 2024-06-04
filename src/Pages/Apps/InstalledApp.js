36e898643ced7ed07f3cacf820b40b2a739599aa
V1vek-kushwaha <vivekkushwaha520@gmail.com>
2024-06-04 19:50:37 +0530

    active tab bg added

--- src/Pages/Apps/InstalledApp.js
+++ src/Pages/Apps/InstalledApp.js
@@ -13,54 +13,51 @@ const InstalledApp = () => {
 
   return (
     <>
-      <div className="Apps-container">
-        <AppsRoutesHeader />
-        <div className="appsComponentsContainer flex">
-          <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 mt-4 md:mt-8 gap-[7px] md:gap-8">
-            <div className="appBox mb-4 flex flex-col justify-center">
-              <span className="appBoxicon">
-                <img
-                  className="w-12 md:w-16 md:h-16 lg:w-20 lg:h-20 h-12 m-auto"
-                  src={Whatsapp}
-                  alt="img"
-                />
-              </span>
-              <div className="appBoxDes flex flex-col justify-center">
-                <h3 className="text-md font-semibold">Whatsapp</h3>
-                <p className="text-slate-500 text-sm ">145.76mb</p>
-              </div>
+      <div className="appsComponentsContainer flex">
+        <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 mt-4 md:mt-8 gap-[7px] md:gap-8">
+          <div className="appBox mb-4 flex flex-col justify-center">
+            <span className="appBoxicon">
+              <img
+                className="w-12 md:w-16 md:h-16 lg:w-20 lg:h-20 h-12 m-auto"
+                src={Whatsapp}
+                alt="img"
+              />
+            </span>
+            <div className="appBoxDes flex flex-col justify-center">
+              <h3 className="text-md font-semibold">Whatsapp</h3>
+              <p className="text-slate-500 text-sm ">145.76mb</p>
             </div>
-            {appData && appData.length > 0 ? (
-              appData.map((v, i) => (
-                <Modal>
-                  <Modal.Toggle toggleName="Upgrade">
-                    <div
-                      key={i}
-                      className="appBox mb-4 flex flex-col justify-center"
-                    >
-                      <span className="appBoxicon">
-                        {/* <span className="appBoxicon">{v.icon}</span> */}
-                        <img
-                          className="w-12 md:w-16 md:h-16 lg:w-20 lg:h-20 h-12 m-auto"
-                          src={v.icon}
-                          alt="img"
-                        />
-                      </span>
-                      <div className="appBoxDes flex flex-col justify-center">
-                        <h3 className="text-md font-semibold">{v.title}</h3>
-                        <p className="text-slate-500 text-sm ">{v.size}</p>
-                      </div>
-                    </div>
-                  </Modal.Toggle>
-                  <Modal.Window windowName="Upgrade">
-                    <Upgrade />
-                  </Modal.Window>
-                </Modal>
-              ))
-            ) : (
-              <h3>Data Not Found</h3>
-            )}
           </div>
+          {appData && appData.length > 0 ? (
+            appData.map((v, i) => (
+              <Modal>
+                <Modal.Toggle toggleName="Upgrade">
+                  <div
+                    key={i}
+                    className="appBox mb-4 flex flex-col justify-center"
+                  >
+                    <span className="appBoxicon">
+                      {/* <span className="appBoxicon">{v.icon}</span> */}
+                      <img
+                        className="w-12 md:w-16 md:h-16 lg:w-20 lg:h-20 h-12 m-auto"
+                        src={v.icon}
+                        alt="img"
+                      />
+                    </span>
+                    <div className="appBoxDes flex flex-col justify-center">
+                      <h3 className="text-md font-semibold">{v.title}</h3>
+                      <p className="text-slate-500 text-sm ">{v.size}</p>
+                    </div>
+                  </div>
+                </Modal.Toggle>
+                <Modal.Window windowName="Upgrade">
+                  <Upgrade />
+                </Modal.Window>
+              </Modal>
+            ))
+          ) : (
+            <h3>Data Not Found</h3>
+          )}
         </div>
       </div>
     </>
