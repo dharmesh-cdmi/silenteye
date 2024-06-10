4b7de2cea16c5da265ea62179a8d8fc5abcc7ab7
Saad Ullah Khan <saaad.khaan.69@gmail.com>
2024-06-10 16:00:02 +0500

    fixed

--- src/components/network/wifi/WifiMain.js
+++ src/components/network/wifi/WifiMain.js
@@ -18,19 +18,7 @@ function WifiMain() {
       />
       <Tabs />
       <DataTable />
-      <div className="flex justify-center mb-12">
-        <Modal>
-          <Modal.Toggle toggleName="viewMore-keylogger">
-            <Button className="text-[18px] shadow-[0px_0px_56px_0px_#00000014]   font-medium px-[32px]  py-[10px] text-[#101828] text-center mt-7">
-              View More
-              <MdKeyboardDoubleArrowDown size={18} />
-            </Button>
-          </Modal.Toggle>
-          <Modal.Window windowName="viewMore-keylogger">
-            <Upgrade />
-          </Modal.Window>
-        </Modal>
-      </div>
+  
     </Card>
   );
 }
