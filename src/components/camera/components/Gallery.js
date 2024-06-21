c765c70880e23d47a12dbd43f72bae34140b78dd
Saad Ullah Khan <saaad.khaan.69@gmail.com>
2024-06-21 15:21:27 +0500

    camera

--- src/components/camera/components/Gallery.js
+++ src/components/camera/components/Gallery.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 import rect from "../../../assests/images/rect.png"
+import i2 from "../../../assests/images/i2.png"
+import i4 from "../../../assests/images/i4.png"
+import i5 from "../../../assests/images/i5.png"
+import i6 from "../../../assests/images/i6.png"
+import gallery from "../../../assests/images/gallery.png";
+
 
 function Gallery() {
   const images = [
     { src: rect },
-    { src: rect },
-    { src: rect },
-    { src: rect },
-    { src: rect },
-    { src: rect },
+    { src: i2 },
+    { src: gallery },
+    { src: i4 },
+    { src: i5 },
+    { src: i6 },
     { src: rect },
     { src: rect },
     { src: rect },
@@ -17,7 +23,7 @@ function Gallery() {
   return (
     <div>
       <div className="p-4 border-r-2">
-        <h3 className="font-semibold text-xl">Recent Captures</h3>
+        <h3 className="font-semibold text-lg lg:text-xl lg:text-gray-800 text-gray-500">Recent Captures</h3>
       </div>
       <div className="grid grid-cols-3">
         {images.map((img, idx) => {
--- src/components/camera/components/Tabs.js
+++ src/components/camera/components/Tabs.js
@@ -2,29 +2,32 @@ import React from "react";
 import { IoImageOutline } from "react-icons/io5";
 import { IoVideocamOutline } from "react-icons/io5";
 import { PiFilmReelBold } from "react-icons/pi";
+import photo from '../../../assests/images/imggg.png'
+import vid from '../../../assests/images/videooo.png'
+import frame from '../../../assests/images/Framee.png'
 
 
 function Tabs() {
   const contents = [
-    { title: "Total Photos (985.12 k)", d1: "120.10 GB", d2: " of photos",comp:<IoImageOutline className="mr-2 mt-1" />
+    { title: "Total Photos (985.12k)", d1: "120.10 GB", d2: " of photos",comp:<img className="w-6 h-6 mt-0.5 mr-1" src={photo}/>
   },
-    { title: "Total Photos (985.12 k)", d1: "120.10 GB", d2: " of photos",comp:<IoVideocamOutline className="mr-2 mt-1" />
+    { title: "Total Videos (1.25k)", d1: "264.38 GB", d2: " of photos",comp:<img className="w-6 h-6 mt-0,5 mr-1" src={vid}/>
   },
-    { title: "Total Photos (985.12 k)", d1: "120.10 GB", d2: " of photos",comp:<PiFilmReelBold className="mr-2 mt-1" />
+    { title: "Recent Captures (124 files)", d1: "720.35 MB", d2: " in Last 24 Hrs",comp:<img className="w-6 h-6 mt-0.5 mr-1" src={frame} />
   },
   ];
   return (
-    <div className="grid grid-cols-3 border rounded-lg mx-10 mt-10">
+    <div className="grid lg:grid-cols-3 lg:border-t-0 border-t-2 grid-cols-1 lg:border-2 rounded-lg mx-2 mt-2 lg:mt-10">
       {contents.map((content, index) => {
         return (
-          <div key={index} className="border-r-2 p-2">
+          <div key={index} className={`${content.d1==="720.35 MB" ? "lg:border-r-0 border-b-2" : "lg:border-r-2 border-b-2"} p-2`}>
             <div className=" flex">
               {content.comp}
-              <h3 className="font-semibold">{content.title}</h3>
+              <h3 className="font-bold text-lg">{content.title}</h3>
             </div>
-            <p className="text-sm font-semibold">
+            <p className="text-md font-semibold">
               {content.d1}
-              <span className="text-gray-600 font-normal">{content.d2}</span>
+              <span className="text-gray-600 font-normal text-base">{content.d2}</span>
             </p>
           </div>
         );
