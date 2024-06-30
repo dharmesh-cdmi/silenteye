e20532a564d7b8798c2890c33a56201d2470e1df
Saad Ullah Khan <saaad.khaan.69@gmail.com>
2024-06-30 12:38:26 +0500

    ac

--- src/components/camera/CameraMain.js
+++ src/components/camera/CameraMain.js
@@ -12,38 +12,33 @@ import CallsHead from "../Calls/CallsHead";
 function CameraMain() {
   const headerData = {
     location: "All Captures",
-    
     placeHolder: "Search by Photos, People, Places",
   };
   return (
     <Card>
-   <CallsHead
-        heading={"All captures"}
-        placeholder={"Search by App Name"}
-      />      <Tabs />
+      <CallsHead heading={"All captures"} placeholder={"Search by App Name"} />{" "}
+      <Tabs />
       <div className="grid lg:grid-cols-2 grid-cols-1 lg:mx-2 mx-0 lg:my-10 my-0 lg:border-2 border-0 rounded-lg">
         <Gallery />
         <Image />
       </div>
       <div className="lg:flex justify-center hidden">
-      <Modal>
-        <Modal.Toggle toggleName="Update">
-          <div className="flex justify-center align middle my-10  ">
-            <button className="mx-auto border-2 shadow-lg px-5 py-1.5 rounded-lg flex ">
-              View More
-              <MdKeyboardDoubleArrowDown className="mt-1 h-4 w-4" />
-            </button>
-          </div>
-        </Modal.Toggle>
-        <Modal.Window windowName="Update">
-          <Upgrade />
-        </Modal.Window>
-      </Modal>
+        <Modal>
+          <Modal.Toggle toggleName="Update">
+            <div className="flex justify-center align middle my-10  ">
+              <button className="mx-auto border-2 shadow-lg px-5 py-1.5 rounded-lg flex ">
+                View More
+                <MdKeyboardDoubleArrowDown className="mt-1 h-4 w-4" />
+              </button>
+            </div>
+          </Modal.Toggle>
+          <Modal.Window windowName="Update">
+            <Upgrade />
+          </Modal.Window>
+        </Modal>
       </div>
     </Card>
   );
 }
-
-
 
 export default CameraMain;
